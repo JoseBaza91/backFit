9a88a051fd0c03e8ebcb149c78d61491995fdd95
JoseBaza91 <musulmanco@gmail.com>
2024-09-10 20:22:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+/*esto es para levantar el servidor */
+const Server = require("./server/server");
+const server = new Server();
+server.listen();
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+class Server {
+  constructor() {
+    this.app = express();
+  }
+  listen() {
+    this.app.listen(3001, () => {
+      console.log("server ok", 3001);
+    });
+  }
+}
+/*export default */
+module.exports = Server;
